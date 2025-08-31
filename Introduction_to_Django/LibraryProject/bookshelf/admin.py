37a9290215febba28babe60e77a6eee61613bbc8
ronniedev123 <rontechconsultants@gmail.com>
2025-08-31 22:21:29 +0300

    files

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Book   # 👈 this is what the checker is looking for
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "publication_year")
+    list_filter = ("publication_year", "author")
+    search_fields = ("title", "author")
+
+admin.site.register(Book, BookAdmin)
+
 
 # Register your models here.
