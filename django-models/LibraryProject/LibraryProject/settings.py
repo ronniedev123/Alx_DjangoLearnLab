eabe5903072db466ddedd99ee56d72854f459b91
ronniedev123 <rontechconsultants@gmail.com>
2025-09-12 12:44:58 +0300

    new files

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'relationship_app'
 ]
 
+LOGIN_REDIRECT_URL = '/'       
+LOGOUT_REDIRECT_URL = '/'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import register_view
 from . import views
-from .views import list_books
+
 
 urlpatterns = [
     path('books/', views.book_list, name='book_list'),
     path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
-    path("register/", views.register_view, name="register"),
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
+    path('register/', register_view, name='register'),
+    path('login/', 
+         auth_views.LoginView.as_view(
+         template_name="relationship_app/login.html"),
+         name='login'),
+    path('logout/',
+         auth_views.LogoutView.as_view(
+         template_name="relationship_app/logout.html"),
+         name='logout'),
 ]
