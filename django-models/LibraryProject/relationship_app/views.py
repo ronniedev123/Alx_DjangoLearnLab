c4b4a096635141ed355e92377e83f3c9006769e2
ronniedev123 <rontechconsultants@gmail.com>
2025-09-12 11:50:57 +0300

    updated

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -39,7 +39,10 @@ def logout_view(request):
 
 def book_list(request):
     books = Book.objects.all()
-    return render(request, "relationship_app/templates/list_books.html", {"books": books})
+    return render(
+           request,
+           "relationship_app/list_books.html",
+           {"books": books})
 
 
 class LibraryDetailView(DetailView):
