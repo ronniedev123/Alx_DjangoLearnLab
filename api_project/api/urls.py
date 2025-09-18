f56a1018de9c86b97263abd2400f815a41929e6a
ronniedev123 <rontechconsultants@gmail.com>
2025-09-18 10:45:57 +0300

    modified

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import BookListCreateAPIView
+from .views import BookListAPIView
 
 urlpatterns = [
-    path('books/', BookListCreateAPIView.as_view(), name='book-list-create'),
+    path('books/', BookListAPIView.as_view(), name='book-list-create'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-class BookListCreateAPIView(generics.ListCreateAPIView):
+class BookListAPIView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
