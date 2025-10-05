d3920bcf99af81f7bc84cff0a1eb5e0d3096f66d
ronniedev123 <rontechconsultants@gmail.com>
2025-10-05 12:05:01 +0300

    modified forms

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,8 +2,10 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import Textarea, TextInput
 from .models import Profile
 from .models import Post
+from .models import Comment
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -29,4 +31,20 @@ class PostForm(forms.ModelForm):
         widgets = {
             "title": forms.TextInput(attrs={"placeholder": "Post title", "class": "form-control"}),
             "content": forms.Textarea(attrs={"placeholder": "Write your post...", "class": "form-control", "rows": 8}),
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["content"]
+        widgets = {
+            "content": forms.Textarea(attrs={
+                "rows": 3,
+                "placeholder": "Add a comment...",
+                "class": "form-control"
+            }),
+        }
+        labels = {
+            "content": ""
         }
